d0ec936e38cac198e1d23919880400226d1ca067
wybcitgs <wybcitgs@users.noreply.github.com>
2026-09-25 21:34:49 +0800

    删除 week00 的 hello 示例文件

--- week00-setup/hello/Hello.java
+++
@@ -1,6 +0,0 @@
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello, Java 21!");
-        System.out.println("当前 JDK 版本: " + System.getProperty("java.version"));
-    }
-}
